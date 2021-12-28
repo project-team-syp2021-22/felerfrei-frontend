9774fddd0bea41873a9ba2ae20b5497ff5dee9a2
Florian Kainrath <flo.kainrath@gmail.com>
2021-12-28 13:44:06 +0100

    authprovider finished login

--- components/authExample.jsx
+++ components/authExample.jsx
@@ -1,22 +1,38 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { useAuth } from './authprovider';
 import Button from 'react-bootstrap/Button';
+import { Container, Form } from 'react-bootstrap';
+import axios from 'axios';
 
 export default function AuthExample() {
 	const { user, login } = useAuth();
 
+	const emailRef = useRef();
+	const passwordRef = useRef();
+
 	function log() {
-		login('hallo', 'hallo');
+		const email = emailRef.current.value;
+		const password = passwordRef.current.value;
+
+		login(email, password);
 	}
 
 	return (
 		<div>
-			{user && <h1>Welcome {user.username}</h1>}
+			{user && <h1>Welcome {user.email}</h1>}
 
 			{!user && (
-				<Button variant="primary" onClick={log}>
-					Login
-				</Button>
+				<Container className="center mt-5" style={{ maxWidth: "400px" }}>
+					<Form>
+						<Form.Group>
+							<Form.Control ref={emailRef} type="email" placeholer="enter your email" />
+							<Form.Control ref={passwordRef} type="password" placeholder="enter your password" className="mt-4" />
+						</Form.Group>
+					</Form>
+					<Button variant="primary" className='mt-4' onClick={log}>
+						Login
+					</Button>
+				</Container>
 			)}
 		</div>
 	);
