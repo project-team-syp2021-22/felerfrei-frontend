afe559e98771e5ea316258289584a11649fa20ad
KainFlorian <flo.kainrath@gmail.com>
2022-01-06 14:47:37 +0100

    removed rounded corners

--- pages/login.js
+++ pages/login.js
@@ -25,12 +25,13 @@ export default function Login() {
                     <h3>Felerfrei - Login</h3>
                     <Form onSubmit={onSubmit}>
                         <InputGroup className="mb-4 mt-4">
-                            <InputGroup.Text id="basic-addon1" className="bg-transparent">
+                            <InputGroup.Text id="basic-addon1" className="bg-transparent rounded-0">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person" viewBox="0 0 16 16">
                                     <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10z" />
                                 </svg>
                             </InputGroup.Text>
                             <FormControl
+                                className="rounded-0"
                                 placeholder="Username"
                                 aria-label="Username"
                                 aria-describedby="basic-addon1"
@@ -38,12 +39,13 @@ export default function Login() {
                             />
                         </InputGroup>
                         <InputGroup className="mb-4">
-                            <InputGroup.Text id="basic-addon2" className="bg-transparent">
+                            <InputGroup.Text id="basic-addon2" className="bg-transparent rounded-0">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-lock" viewBox="0 0 16 16">
                                     <path d="M8 1a2 2 0 0 1 2 2v4H6V3a2 2 0 0 1 2-2zm3 6V3a3 3 0 0 0-6 0v4a2 2 0 0 0-2 2v5a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V9a2 2 0 0 0-2-2zM5 8h6a1 1 0 0 1 1 1v5a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1V9a1 1 0 0 1 1-1z" />
                                 </svg>
                             </InputGroup.Text>
                             <FormControl
+                                className="rounded-0"
                                 type="password"
                                 placeholder="Password"
                                 aria-label="Password"
@@ -53,7 +55,7 @@ export default function Login() {
                         </InputGroup>
 
 
-                        <Button className="w-100" variant="outline-dark" type="submit" size={"md"} style={{ transition: '0.5s' }}>
+                        <Button className="w-100 rounded-0" variant="outline-dark" type="submit" size={"md"} style={{ transition: '0.5s' }}>
                             Anmelden
                         </Button>
                     </Form>
