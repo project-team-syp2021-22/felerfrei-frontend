27bb59839dd64590869152f6461066bf3a62645b
Vivi <v.koller@htlstp.at>
2022-01-26 10:35:53 +0100

    halbwegs

--- pages/profile.js
+++ pages/profile.js
@@ -10,40 +10,47 @@ import {useAuth} from "../components/authprovider";
 To do:
 senden an Server
 Werte vom Server
-anzeigen, dass Passwörter nicht geändert werden MÜSSEN
 
-kann man bei der Passwortstärke eine andere Sprache anzeigen?
-auch überprüfen, ob das Passwort gut genug ist?
  */
 
 export default function Profile() {
     let sonderzeichen = "(°_^}"
     const passwordVisibility = useRef();
-    const passwordConfirmVisibility = useRef();
+    // const passwordConfirmVisibility = useRef();
 
     const [passwordIsHidden, setPasswordIsHidden] = useState(true);
     const [loginError, setUpdateError] = useState();
     const [password, setPassword] = useState();
 
     const passwordRef = useRef();
-    const passwordConfirmRef = useRef();
+    // const passwordConfirmRef = useRef();
     const emailRef = useRef();
     const firstNameRef = useRef();
     const lastNameRef = useRef();
     const phoneRef = useRef();
     const router = useRouter();
+    const [timesLoaded, setTimesLoaded] = useState(0)
 
     let {signup} = useAuth();
 
 
     let classRequired = "required"
 
+
     function inputExists() {
         return (checkForRequiredInput(firstNameRef)
             && checkForRequiredInput(lastNameRef)
             && checkForRequiredInput(emailRef)
             && checkForRequiredInput(phoneRef)
+            && checkForRequiredInput(passwordRef)
         )
+    }
+
+    function setInputs() {
+        firstNameRef.current.value = "Victoria"
+        lastNameRef.current.value = "Koller"
+        // emailRef.current.value = "htaskuf@asguis"
+        // phoneRef.current.value = "431717171771"
     }
 
     function checkForRequiredInput(state) {
@@ -52,7 +59,6 @@ export default function Profile() {
 
     function handlePasswordVisibility() {
         let passwordTag = passwordRef.current;
-        let passwordConfirmTag = passwordConfirmRef.current;
         if (passwordIsHidden) {
             passwordVisibility.current.innerHTML = `
             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-eye-slash" viewBox="0 0 16 16">
@@ -68,10 +74,9 @@ export default function Profile() {
             </svg>
             `
         }
-        passwordConfirmVisibility.current.innerHTML = passwordVisibility.current.innerHTML;
         setPasswordIsHidden(!passwordIsHidden);
         passwordTag.type = passwordIsHidden ? "text" : "password";
-        passwordConfirmTag.type = passwordIsHidden ? "text" : "password";
+
     }
 
     async function setNewValues() {
@@ -81,19 +86,16 @@ export default function Profile() {
 
         let email = emailRef.current.value;
         let password = passwordRef.current.value;
-        let passwordConfirm = passwordConfirmRef.current.value;
         let firstname = firstNameRef.current.value;
         let lastname = lastNameRef.current.value;
         let telephone = phoneRef.current.value;
 
-        if (password !== passwordConfirm) {
-            return setUpdateError("Passwörter ungleich!");
-        }
-
         setUpdateError(null);
         try {
             alert(firstname + " " + lastname + " " + email + " " + telephone + " " + password)
-            // await signup(email, password, firstname, lastname, telephone);
+            //       await signup(email, password, firstname, lastname, telephone);
+
+            passwordRef.current.value = ""
         } catch (error) {
             setUpdateError(error.message);
             return;
@@ -118,6 +120,7 @@ export default function Profile() {
                                 placeholder="Vorname"
                                 aria-label="Vorname"
                                 ref={firstNameRef}
+                                defaultValue="Vici"
                                 required
                             />
                         </InputGroup>
@@ -167,7 +170,7 @@ export default function Profile() {
                             <FormControl
                                 className="rounded-0 border-0 border-bottom border-dark"
                                 type="password"
-                                placeholder="Password ändern"
+                                placeholder="Passwort"
                                 aria-label="Password"
                                 aria-describedby="input-group-addon"
                                 ref={passwordRef}
@@ -191,34 +194,6 @@ export default function Profile() {
                             </div>
                         </InputGroup>
 
-                        <InputGroup className="mb-4">
-                            <FormControl
-                                className="rounded-0 border-0 border-bottom border-dark"
-                                type="password"
-                                placeholder="Neues Passwort bestätigen"
-                                id="Password1"
-                                aria-label="Password"
-                                aria-describedby="input-group-addon"
-                                ref={passwordConfirmRef}
-                            />
-                            <div className="input-group-addon border-bottom border-dark mt-1"
-                                 onClick={handlePasswordVisibility}
-                                 ref={passwordConfirmVisibility}>
-                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-                                     className="bi bi-eye-slash" viewBox="0 0 16 16">
-                                    <path
-                                        d="M13.359 11.238C15.06 9.72 16 8 16 8s-3-5.5-8-5.5a7.028 7.028 0 0 0-2.79.588l.77.771A5.944 5.944 0 0 1 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13.134 13.134 0 0 1 14.828 8c-.058.087-.122.183-.195.288-.335.48-.83 1.12-1.465 1.755-.165.165-.337.328-.517.486l.708.709z"/>
-                                    <path
-                                        d="M11.297 9.176a3.5 3.5 0 0 0-4.474-4.474l.823.823a2.5 2.5 0 0 1 2.829 2.829l.822.822zm-2.943 1.299.822.822a3.5 3.5 0 0 1-4.474-4.474l.823.823a2.5 2.5 0 0 0 2.829 2.829z"/>
-                                    <path
-                                        d="M3.35 5.47c-.18.16-.353.322-.518.487A13.134 13.134 0 0 0 1.172 8l.195.288c.335.48.83 1.12 1.465 1.755C4.121 11.332 5.881 12.5 8 12.5c.716 0 1.39-.133 2.02-.36l.77.772A7.029 7.029 0 0 1 8 13.5C3 13.5 0 8 0 8s.939-1.721 2.641-3.238l.708.709zm10.296 8.884-12-12 .708-.708 12 12-.708.708z"/>
-                                </svg>
-                            </div>
-                        </InputGroup>
-
-                        <InputGroup>
-                            <PasswordStrengthBar password={password} className="w-100"/>
-                        </InputGroup>
 
                         <Button onClick={setNewValues}
                                 className="w-100 rounded-0 mt-4"
@@ -233,7 +208,10 @@ export default function Profile() {
                     <div className="mt-4">
                         <Link href="/login">Account deaktivieren</Link>
                         <br/>
-                        <Link href="/profile">Account löschen</Link>
+                        <Link href="/login">Account löschen</Link>
+                        <br/>
+                        <Link href="/login">Passwort vergessen</Link>
+
                     </div>
                     {
                         loginError &&
