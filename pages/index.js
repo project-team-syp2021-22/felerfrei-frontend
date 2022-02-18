f30351d06a023f8da73f8d846525624f14498553
Florian Kainrath <flo.kainrath@gmail.com>
2022-02-18 16:33:03 +0100

    fixed image bug

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,9 @@ export default function Index() {
 
   return (
     <>
-      <Image src={background} alt="background" />
+      <div style={{ zIndex: "-10" }}>
+        <Image src={background} alt="background" />
+      </div>
       <div className="d-flex justify-content-center align-content-center mt-5 w-100 mb-5">
         <div className="w-75 d-flex justify-content-center mt-5">
           <div className="d-flex flex-column align-items-center h-100 me-5 w-50">
