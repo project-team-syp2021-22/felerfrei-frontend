baed7770a689609d60e9068a28989e09349b82c0
Florian Kainrath <flo.kainrath@gmail.com>
2022-02-19 15:56:58 +0100

    added content to index page

--- pages/index.js
+++ pages/index.js
@@ -25,8 +25,8 @@ export default function Index() {
             </div>
 
             <div>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.
+              At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
             </div>
           </div>
         </div>
@@ -42,11 +42,39 @@ export default function Index() {
                 />
               </div>
             </div>
-            <img width={500} src="https://images.unsplash.com/photo-1599651993975-30a482e26467?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80" />
+            <img width={550} src="https://images.unsplash.com/photo-1599651993975-30a482e26467?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80" />
           </div>
         </div>
       </div>
-
+      <Divider />
+      <div className="d-flex justify-content-center align-content-center mt-3 w-100 mb-5">
+        <div className="w-75 d-flex justify-content-center mt-5">
+          <div className="d-flex flex-row w-100 justify-content-evenly">
+            <div className="d-flex flex-column">
+              <img width={350} src="https://images.unsplash.com/photo-1594081500655-8cf4a358b934?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80" />
+              <div className="text-break" style={{ width: "350px" }}>
+                <div className={`w-100 ${styles.subheading} mt-1`}>
+                  content
+                </div>
+                <div className={styles.text}>
+                  Lorem ipsum dolor sit amet, consetetur sadipscing elitr.
+                </div>
+              </div>
+            </div>
+            <div className="d-flex flex-column">
+              <img width={350} src="https://images.unsplash.com/photo-1590529989936-f6efdf774c23?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80" />
+              <div className="text-break" style={{ width: "350px" }}>
+                <div className={`w-100 ${styles.subheading} mt-1`}>
+                  content
+                </div>
+                <div className={styles.text}>
+                  Lorem ipsum dolor sit amet, consetetur sadipscing elitr.
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
       <Footer />
     </>
   );
