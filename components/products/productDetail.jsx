85d9e6cae4cfc3fea1d6568346cf9d4581a0cce0
Florian Kainrath <flo.kainrath@gmail.com>
2022-03-08 20:33:11 +0100

    refactoring

--- components/products/productDetail.jsx
+++ components/products/productDetail.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Button, Modal, Container } from "react-bootstrap";
+import { Button, Modal } from "react-bootstrap";
 import ImageSlider from "./imageSlider";
 // import styles from '../../styles/productListStyles.module.css';
 import styles from '../../styles/products/productListStyles.module.css';
