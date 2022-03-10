4f73ef9aca694037adb51f622a42a3bf920fb0ab
Florian Kainrath <flo.kainrath@gmail.com>
2022-03-10 20:26:05 +0100

    added hover animation

--- pages/contact.js
+++ pages/contact.js
@@ -1,7 +1,7 @@
 import React from "react";
 import background from '../public/start.svg';
 import Image from "next/image";
-import {motion} from "framer-motion";
+import { motion } from "framer-motion";
 import Divider from "../components/divider";
 import FadeInView from "../components/animation/inview";
 import Link from 'next/link';
@@ -23,9 +23,9 @@ export default function Contacts() {
                     // type: "spring",
                 }
             }}
-            style={{zIndex: "-10"}}
+            style={{ zIndex: "-10" }}
         >
-            <Image src={background} alt="background"/>
+            <Image src={background} alt="background" />
         </motion.div>
         <FadeInView>
             <div className="d-flex justify-content-center align-content-center mt-3 w-100 mb-5">
@@ -36,28 +36,37 @@ export default function Contacts() {
                                 <h1 className={styles.textCentering}><b>Kontaktieren Sie mich</b></h1>
                                 <h4 className={styles.textCentering}>Unter der Woche täglich,</h4>
                                 <h4 className={styles.textCentering}>von 7-12 und von 13-16 Uhr</h4>
-                                <br/>
+                                <br />
                                 <h3 className={styles.textCentering}><b>FELERFREI</b></h3>
                                 <h3 className={styles.textCentering}>Unterm Tisch 3</h3>
                                 <h3 className={styles.textCentering}>1234 Irgendwo</h3>
-                                //todo
-                                <h3 className={styles.linking}>T:<Link className={styles.hoverUnderlineAnimation}
-                                                                       href="tel:0664 1234567">0664 1234567 </Link>
+                                <h3 className={styles.linking}>
+                                    <span className={styles.hoverUnderlineAnimation}>
+                                        T:
+                                        <Link href="tel:0664 1234567">
+                                            0664 1234567
+                                        </Link>
+                                    </span>
                                 </h3>
-                                <h3 className={styles.linking}>E:<Link className={styles.hoverUnderlineAnimation}
-                                                                       href="mailto:office@felerfrei.at">office@felerfrei.at </Link>
+                                <h3 className={styles.linking}>
+                                    <span className={styles.hoverUnderlineAnimation}>
+                                        E:
+                                        <Link href="mailto:office@felerfrei.at">
+                                            office@felerfrei.at
+                                        </Link>
+                                    </span>
                                 </h3>
                             </div>
                         </div>
                         <img width={550} alt="Bild"
-                             src="https://images.unsplash.com/photo-1599651993975-30a482e26467?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"/>
+                            src="https://images.unsplash.com/photo-1599651993975-30a482e26467?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80" />
                     </div>
                 </div>
             </div>
-            <Divider/>
+            <Divider />
         </FadeInView>
 
-        <Footer/>
+        <Footer />
 
     </>
 }
