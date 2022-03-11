543ae43313aed7d483168062282000df390a0c88
KainFlorian <flo.kainrath@gmail.com>
2022-03-11 10:27:10 +0100

    added admin page

---
+++ components/adminPage.jsx
@@ -0,0 +1,37 @@
+import axios from "axios";
+import { useRouter } from "next/router";
+import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
+import { useAuth } from "./authprovider";
+import { API_URL } from "./constants";
+
+function AdminPage({children}) {
+  const { userToken } = useAuth();
+  const [loading, setLoading] = useState(true);
+
+  let router = useRouter();
+
+  if (!userToken) {
+    return <div>You are not logged in</div>;
+  }
+
+  useEffect(async () => {
+    await axios
+      .get(`${API_URL}/auth/isAdmin`, {
+        headers: { Authorization: `Bearer ${userToken.token}` },
+      })
+      .catch((err) => {
+        router.push("/");
+      });
+    setLoading(false);
+  }, []);
+
+  return (
+    <div>
+        {!loading && children}
+    </div>
+  );
+}
+
+export default AdminPage;
---
+++ pages/admin.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import AdminPage from '../components/adminPage';
+
+function Admin() {
+  return (
+    <AdminPage>
+        <div className="mt-5">
+        <h1>Admin</h1>
+        </div>
+    </AdminPage>
+  )
+}
+
+export default Admin;
