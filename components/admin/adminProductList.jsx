80241bafa953cb00929b43851fcc7090caee38eb
Florian Kainrath <flo.kainrath@gmail.com>
2022-03-14 16:02:03 +0100

    new product function works

--- components/admin/adminProductList.jsx
+++ components/admin/adminProductList.jsx
@@ -4,8 +4,7 @@ import AdminPage from './adminPage';
 import { API_URL } from '../constants';
 import { useAuth } from '../authprovider';
 import AdminProduct from './adminProduct';
-import { Table, Button, Modal, Form } from 'react-bootstrap';
-import { useRouter } from 'next/router';
+import { Table, Button, Modal, Form, Alert } from 'react-bootstrap';
 
 let pageIndex = 0;
 function AdminProductList() {
@@ -13,15 +12,35 @@ function AdminProductList() {
     const { userToken } = useAuth();
     const [products, setProducts] = useState([]);
     const [loading, setLoading] = useState(false);
-    const [last, setLast] = useState(false);
-    const [showAddModal, setShowAddModal] = useState(false);
 
-    async function loadProjects(index) {
-        if (last) {
+    let last = useRef(false);
+
+    // const [last, setLast] = useState(false);
+
+    const [showAddModal, setShowAddModal] = useState(false);
+    const [addError, setAddError] = useState();
+
+    let nameRef = useRef();
+    let priceRef = useRef();
+    let descriptionRef = useRef();
+    let materialRef = useRef();
+
+    useEffect(() => {
+        if (showAddModal == true) {
+            return;
+        }
+        console.log("here")
+        pageIndex = 0;
+        last.current = false;
+        loadProducts(pageIndex, true);
+    }, [showAddModal]);
+
+    async function loadProducts(index, reload = false) {
+        if (last.current) {
             return;
         }
         setLoading(true);
-        axios.get(`${API_URL}/admin/products?size=20&page=${index}`, {
+        await axios.get(`${API_URL}/admin/products?size=20&page=${index}`, {
             headers: {
                 Authorization: `Bearer ${userToken.token}`
             }
@@ -30,30 +49,66 @@ function AdminProductList() {
                 console.log(res.data)
                 // setProducts(res.data);
                 if (res.data.last) {
-                    setLast(true);
+                    last.current = true;
                 }
                 console.log(res.data.content, pageIndex);
-                setProducts([...products, ...res.data.content]);
+                if (reload) {
+                    setProducts([...res.data.content]);
+                } else {
+                    setProducts([...products, ...res.data.content]);
+                }
             })
         setLoading(false);
     }
 
     function showMore() {
-        loadProjects(pageIndex += 1);
+        loadProducts(pageIndex += 1);
     }
-
-    useEffect(() => {
-        loadProjects(pageIndex);
-    }, []);
 
     function handleClose() {
         setShowAddModal(false);
     }
 
+    function addProduct() {
+        let name = nameRef.current.value;
+        let price = priceRef.current.value;
+        let description = descriptionRef.current.value;
+        let material = materialRef.current.value;
+
+        setAddError(null);
+
+        if (!name || !price || !description || !material) {
+            setAddError("Please fill out all fields");
+            return;
+        }
+
+        axios.post(`${API_URL}/admin/addProduct`, {
+            name,
+            price,
+            description,
+            material
+        }, {
+            headers: {
+                Authorization: `Bearer ${userToken.token}`
+            }
+        })
+            .then(res => {
+                setShowAddModal(false);
+            })
+            .catch(err => {
+                console.log(err);
+                setAddError(err.response);
+            });
+    }
+
     return (
         <AdminPage>
             <div className="w-100 d-flex justify-content-center mt-3 mb-3">
-                <Button variant="dark" onClick={() => setShowAddModal(true)}>
+                <Button variant="dark"
+                    onClick={() => {
+                        setShowAddModal(true);
+                        setAddError(null);
+                    }}>
                     Neues Produkt
                 </Button>
             </div>
@@ -83,7 +138,7 @@ function AdminProductList() {
                     ))}
                 </tbody>
             </Table>
-            {!last &&
+            {!last.current &&
 
                 <Button variant='dark' onClick={showMore}>
                     {loading ? 'Lade...' : 'Mehr laden'}
@@ -103,16 +158,24 @@ function AdminProductList() {
                 </Modal.Header>
                 <Modal.Body>
                     <Form.Group>
-                        <Form.Control className="rounded-0 border-0 border-bottom" type="text" placeholder="Name" />
-                        <Form.Control className="rounded-0 border-0 border-bottom mt-3" type="textarea" placeholder="description" />
-                        <Form.Control className="rounded-0 border-0 border-bottom mt-3" type="number" placeholder="Price" />
-                        <Form.Control className="rounded-0 border-0 border-bottom mt-3" type="text" placeholder="Material" />
+                        <Form.Control className="rounded-0 border-0 border-bottom" ref={nameRef} type="text" placeholder="Name" />
+                        <Form.Control className="rounded-0 border-0 border-bottom mt-3" ref={descriptionRef} as="textarea" rows={3} placeholder="description" />
+                        <Form.Control className="rounded-0 border-0 border-bottom mt-3" ref={priceRef} type="number" placeholder="Price" />
+                        <Form.Control className="rounded-0 border-0 border-bottom mt-3" ref={materialRef} type="text" placeholder="Material" />
                     </Form.Group>
                 </Modal.Body>
                 <Modal.Footer>
-                    <Button variant="dark">
-                        Hinzufügen
-                    </Button>
+                    <div className="d-flex flex-column w-100">
+                        <Button variant="dark" onClick={addProduct}>
+                            Hinzufügen
+                        </Button>
+                        <br />
+                        {addError &&
+                            <Alert className="w-100" variant="danger">
+                                {addError}
+                            </Alert>
+                        }
+                    </div>
                 </Modal.Footer>
             </Modal>
         </AdminPage>
