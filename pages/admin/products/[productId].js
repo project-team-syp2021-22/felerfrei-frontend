977948eca23e0245bd2dcf50176e0a621afd4072
KainFlorian <flo.kainrath@gmail.com>
2022-04-06 10:58:21 +0200

    removing images

--- pages/admin/products/[productId].js
+++ pages/admin/products/[productId].js
@@ -55,6 +55,16 @@ function Product() {
         })
     }
 
+    function deleteImage(imageId) {
+        axios.post(`${API_URL}/admin/removeImage/${productId}/${imageId}`, {}, {
+            headers: {
+                Authorization: `Bearer ${userToken.token}`
+            }
+        }).then(res => {
+            setImages(images.filter(image => image.id !== imageId));
+        });
+    }
+
     return (
         <AdminPage>
             {error && <>
@@ -106,16 +116,29 @@ function Product() {
                                 Ändern
                             </Button>
                         </div>
+                        <div className="d-flex justify-content-center">
+                            <Button variant='dark' className="mt-3 rounded-0 w-50" onClick={handleChange}>
+                                Add Image
+                            </Button>
+                        </div>
                         <div className="mt-5">
                             {images.map(image => {
                                 console.log(`${API_URL}/api/image/${image.id}`)
                                 return (
-                                    <div className="m-3 d-inline">
+                                    <div className="m-3 d-block">
                                         <img
                                             style={{ maxWidth: "200px" }}
                                             src={`${API_URL}/api/image/${image.id}`}
                                             alt="Product"
                                         />
+                                        <div className="ms-5 d-inline">
+                                            <Button variant='danger' onClick={() => deleteImage(image.id)}>
+                                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-trash" viewBox="0 0 16 16">
+                                                    <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z" />
+                                                    <path fillRule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z" />
+                                                </svg>
+                                            </Button>
+                                        </div>
                                     </div>
                                 );
                             })}
