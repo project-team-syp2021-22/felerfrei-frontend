8c567cbd8260dd0f6507a838cb78d1bc4061458d
thoma <thomas.hablecker@gmail.com>
2022-05-11 13:54:12 +0200

    Footer

--- pages/cart.js
+++ pages/cart.js
@@ -1,18 +1,19 @@
-import React, { useEffect, useState } from 'react'
-import { Container, Spinner, Button, Alert } from 'react-bootstrap'
-import { useAuth } from '../components/authprovider'
+import React, {useEffect, useState} from 'react'
+import {Container, Spinner, Button, Alert} from 'react-bootstrap'
+import {useAuth} from '../components/authprovider'
 import ShoppingCartItem from "../components/shoppingCartItem.jsx";
 import axios from 'axios';
-import { API_URL } from "../components/constants";
+import {API_URL} from "../components/constants";
 import FadeInView from '../components/animation/inview';
 import Link from 'next/link';
 import styles from '../styles/cart/cart.module.css';
 import OrderModalPage from "../components/orderModalpage";
+import Footer from "../components/footer";
 
 export default function ShoppingCart() {
 
     const [order, setOrder] = useState();
-    const { userToken } = useAuth();
+    const {userToken} = useAuth();
     const [error, setError] = useState();
     const [loading, setLoading] = useState();
     const [showModal, setShowModal] = useState(false);
@@ -55,89 +56,98 @@ export default function ShoppingCart() {
     }
 
     return (
-        <>
-            {loading &&
-                <div className="d-flex w-100 h-100 justify-content-center align-items-center">
-                    <div>
-                        <Spinner />
-                    </div>
-                </div>
-            }
-            {!loading &&
-                <FadeInView>
-                    <div className="d-flex d-table justify-content-center align-items-center h-100 mt-5">
-                        <div className="w-100 d-table-cell align-middle" style={{
-                            display: "table-cell",
-                            verticalAlign: "middle",
-                        }}>
-                            <Container>
-                                <div className="justify-content-center border-bottom border-dark mb-3">
-                                    <h2 className="fw-bold">Warenkorb</h2>
-                                </div>
-                                {order && !order.empty && order.order.orderContents.map((item, index) => {
-                                    return (
-                                        <ShoppingCartItem
-                                            key={item.id}
-                                            productId={item.product.id}
-                                            name={item.product.name}
-                                            quantity={item.amount}
-                                            extra={item.extrawurscht}
-                                            image={item.product.images[0].id}
-                                            price={item.product.price}
-                                            id={item.id}
-                                        />
-                                    );
-                                })}
-
-                                {!loading &&
-                                    <OrderModalPage
-                                        show={showModal}
-                                        userToken={userToken}
-                                        onHide={() => setShowModal(false)}
-                                    />
-                                }
-                                {
-                                    order && order.empty &&
-                                    <>
-                                        <div className="w-100 d-flex flex-column justify-content-center">
-
-                                            <Alert variant="warning" className="w-75 rounded-0">
-                                                Dein Warenkorb ist leer.
-                                            </Alert>
-                                            <div>
-                                                <Link href="/shop">
-                                                    Besuche unseren Shop.
-                                                </Link>
-                                            </div>
-                                        </div>
-                                    </>
-                                }
-
-                                {order && !order.empty &&
-                                    <>
-                                        <div className="w-100 d-flex justify-content-center">
-
-                                            <Button onClick={orderNow}
-                                                className="w-50 rounded-0 mt-4"
-                                                variant="dark"
-                                                size={"md"}
-
-                                                style={{ transition: '0.5s' }}>
-                                                Bestellen
-                                            </Button>
-                                        </div>
-
-                                        <div className={`mt-4 ${styles.fitContent}`}>
-                                            <div onClick={deleteAll} role="button">Warenkorb leeren</div>
-                                        </div>
-                                    </>
-                                }
-                            </Container>
+        <div style={{height: "calc(100vh - 83px)", alignContent: "space-evenly"}}>
+            <div style={{height: "calc(100% - 290px)"}}>
+                {loading &&
+                    <div className="d-flex w-100 h-100 justify-content-center align-items-center">
+                        <div>
+                            <Spinner/>
                         </div>
                     </div>
-                </FadeInView>
-            }
-        </>
+                }
+                {!loading &&
+                    <FadeInView>
+                        <div className="d-flex d-table justify-content-center align-items-center h-100 mt-5">
+                            <div className="w-100 d-table-cell align-middle" style={{
+                                display: "table-cell",
+                                verticalAlign: "middle",
+                            }}>
+                                <Container>
+                                    <div className="justify-content-center border-bottom border-dark mb-3">
+                                        <div className="d-flex flex-row justify-content-between">
+                                            <h2 className="fw-bold">Warenkorb</h2>
+                                            <div className={`m-2 ${styles.fitContent}`}>
+                                                <div onClick={deleteAll} role="button">Warenkorb leeren</div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    {order && !order.empty && order.order.orderContents.map((item, index) => {
+                                        return (
+                                            <ShoppingCartItem
+                                                key={item.id}
+                                                productId={item.product.id}
+                                                name={item.product.name}
+                                                quantity={item.amount}
+                                                extra={item.extrawurscht}
+                                                image={item.product.images[0].id}
+                                                price={item.product.price}
+                                                id={item.id}
+                                            />
+                                        );
+                                    })}
+
+                                    {!loading &&
+                                        <OrderModalPage
+                                            show={showModal}
+                                            userToken={userToken}
+                                            onHide={() => setShowModal(false)}
+                                        />
+                                    }
+                                    {
+                                        order && order.empty &&
+                                        <>
+                                            <div className="w-100 d-flex flex-column justify-content-center">
+
+                                                <Alert variant="warning" className="w-75 rounded-0">
+                                                    Dein Warenkorb ist leer.
+                                                </Alert>
+                                                <div>
+                                                    <Link href="/shop">
+                                                        Besuche unseren Shop.
+                                                    </Link>
+                                                </div>
+                                            </div>
+                                        </>
+                                    }
+
+                                    {order && !order.empty &&
+                                        <>
+                                            <div className="w-100 d-flex justify-content-center">
+
+                                                <Button onClick={orderNow}
+                                                        className="w-50 rounded-0 mt-4 mb-4"
+                                                        variant="dark"
+                                                        size={"md"}
+
+                                                        style={{transition: '0.5s'}}>
+                                                    Bestellen
+                                                </Button>
+                                            </div>
+                                        </>
+                                    }
+                                </Container>
+                            </div>
+                        </div>
+                    </FadeInView>
+                }
+            </div>
+            <div style={{verticalAlign: "bottom"}}>
+                <div style={{height: "320px"}}>
+
+                </div>
+                <Footer/>
+            </div>
+        </div>
     );
 
 }
@@ -151,9 +161,7 @@ export async function getServerSideProps(context) {
                 permanent: false,
                 destination: '/login'
             },
-            props: {
-
-            },
+            props: {},
         };
     }
     return {
